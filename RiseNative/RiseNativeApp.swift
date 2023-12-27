23f3364b059f1dcc8c2dc32393d5aa8b8802c365
kehindeBankole <bankolek1@gmail.com>
2023-12-27 06:17:03 +0100

    Initial Commit

---
+++ RiseNative/RiseNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseNativeApp.swift
+//  RiseNative
+//
+//  Created by kehinde on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RiseNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
